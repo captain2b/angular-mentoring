6ecfd294184a55897825d9bc438422bcd6e16cb5
Mariia Khrustaleva <mariia_khrustaleva@epam.com>
2018-12-09 21:57:35 +0300

    task-2 styles added

--- src/app/courses/components/courses-page/courses-page.component.ts
+++ src/app/courses/components/courses-page/courses-page.component.ts
@@ -27,6 +27,33 @@ export class CoursesPageComponent implements OnInit {
         ' ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis' +
         ' suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur',
     },
+    {
+      id: 3,
+      title: 'Video Course 1',
+      creationDate: new Date('01.01.2018'),
+      duration: 10,
+      description: 'dolorem ipsum, quia dolor sit, amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt,' +
+        ' ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis' +
+        ' suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur',
+    },
+    {
+      id: 4,
+      title: 'Video Course 2',
+      creationDate: new Date(Date.now()),
+      duration: 20,
+      description: 'dolorem ipsum, quia dolor sit, amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt,' +
+        ' ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis' +
+        ' suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur',
+    },
+    {
+      id: 5,
+      title: 'Video Course 1',
+      creationDate: new Date('01.01.2018'),
+      duration: 10,
+      description: 'dolorem ipsum, quia dolor sit, amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt,' +
+        ' ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis' +
+        ' suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur',
+    },
   ];
 
   constructor() {
@@ -37,9 +64,5 @@ export class CoursesPageComponent implements OnInit {
 
   deleteCourse(id: number): void {
     console.log(id);
-  }
-
-  loadCourses(): void {
-    console.log('Load clicked');
   }
 }
