5487adedc4793e29dad1aa8f9734c2c3bfc18db5
Mariia Khrustaleva <Mariia_khrustaleva@epam.com>
2019-01-31 13:00:48 +0300

    task-5 code review fix

--- src/app/courses/components/courses-page/courses-page.component.ts
+++ src/app/courses/components/courses-page/courses-page.component.ts
@@ -36,6 +36,8 @@ export class CoursesPageComponent implements OnInit, OnChanges{
 
   deleteCourse(id: number): void {
     const conf = window.confirm('Do you really want to delete this course?');
-    this.filteredCourses = this.courses = conf &&  this.coursesService.removeCourse(id);
+    if (conf) {
+      this.filteredCourses = this.courses =  this.coursesService.removeCourse(id);
+    }
   }
 }
